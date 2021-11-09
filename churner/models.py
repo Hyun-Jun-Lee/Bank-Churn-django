efcb4f3381881e7b2204e347155febf40f7f0db8
Lee Hyun Jun <mikey1000@naver.com>
2021-11-10 01:57:52 +0900

    fields

--- churner/models.py
+++ churner/models.py
@@ -4,17 +4,15 @@ from django.db import models
 
 class PredResults(models.Model):
     username = models.CharField(max_length=10)
-    Total_trant_Amt = models.IntegerField()
-    Edu_level = models.IntegerField()
-    Income = models.IntegerField() 
-    Total_relationship_cnt = models.IntegerField() 
-    Month_on_book = models.IntegerField() 
-    Customer_age = models.IntegerField() 
-    Contact_cnt_12 = models.IntegerField() 
-    Dependent_count = models.IntegerField() 
-    gender = models.IntegerField() 
+    Total_Trans_Amt  = models.IntegerField()
+    Education_Level  = models.IntegerField()
+    Income_Category  = models.IntegerField(null=True) 
+    Total_Relationship_Count  = models.IntegerField() 
+    Months_on_book  = models.IntegerField() 
+    Customer_Age  = models.IntegerField() 
+    Contacts_Count_12_mon  = models.IntegerField() 
+    Dependent_count  = models.IntegerField() 
+    Gender  = models.IntegerField() 
     result = models.IntegerField() 
-    
-    def __str__(self):
-        return self.result
+
     
