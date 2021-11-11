1d4fe35ff8f5c37e9638448df006afb78867c007
Lee Hyun Jun <mikey1000@naver.com>
2021-11-12 03:01:05 +0900

    heroku

--- bankchurnweb/settings.py
+++ bankchurnweb/settings.py
@@ -16,24 +16,15 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, True)
-)
-
-# reading .env file
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR, '.env')
-)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=env('SECRET_KEY')
+SECRET_KEY=os.environ.get('DJANGO_SECRET_KEY', '(django-insecure-$ry5_2(hlw@ab!$o4z9%9rfaud7%4!1lke&7)kxe1&k8=ka%vq)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
